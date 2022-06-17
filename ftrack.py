1a4768fc7abb227da633e8b13a467cba02defa0f
Robin Sonnabend <robin@fsmpi.rwth-aachen.de>
2022-06-17 21:56:09 +0200

    Start and stop capture if camera output is not used

--- ftrack.py
+++ ftrack.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
 
+import time
+from multiprocessing import Process, Value
+
 import numpy as np
 import cv2
 import mediapipe
 import pyvirtualcam
 
+from inotify.adapters import Inotify
+
 mp_face_detection = mediapipe.solutions.face_detection
 mp_drawing = mediapipe.solutions.drawing_utils
 mp_background = mediapipe.solutions.selfie_segmentation
 mp_pose = mediapipe.solutions.pose
+
+
+def watch_file(filename, mp_value):
+    inotify = Inotify()
+    inotify.add_watch(filename)
+    for event, flags, filename, _ in inotify.event_gen(yield_nones=False):
+        if "IN_OPEN" in flags:
+            mp_value.value += 1
+        if "IN_CLOSE_WRITE" in flags:
+            mp_value.value -= 1
+        if mp_value.value < -10:
+            break
 
 
 def main():
@@ -19,18 +36,17 @@ def main():
     parser.add_argument("--input-size", type=int, nargs=2, default=(1920, 1080), help="Resolution of input video")
     parser.add_argument("--output-size", type=int, nargs=2, default=(1280, 720), help="Resolution of output video")
     parser.add_argument("--fps", type=int, default=30, help="Frames per second for input and output.")
-    parser.add_argument("--smoothness", type=float, default=0.9, help="Tracking smoothing factor.")
+    parser.add_argument("--smoothness", type=float, default=0.95, help="Tracking smoothing factor.")
     parser.add_argument("--blur-background", action="store_true", help="Blur background")
     parser.add_argument("--pose", action="store_true", help="Pose detection")
 
 
     args = parser.parse_args()
 
-    capture = cv2.VideoCapture(args.input)
-
     input_width, input_height = args.input_size
     output_width, output_height = args.output_size
 
+    capture = cv2.VideoCapture(args.input)
     capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
     capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
     capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
@@ -40,56 +56,81 @@ def main():
     if not success:
         print(f"Failed to read frame from source: {args.input}")
         return
+    capture.release()
+
 
     with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detection, mp_background.SelfieSegmentation() as change_bg, mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         with pyvirtualcam.Camera(width=output_width, height=output_height, fps=args.fps, device=args.output) as camera:
             print(f"Using virtual camera: {camera.device}")
 
-            center_x, center_y = input_width / 2, input_height / 2
+            camera.send(np.zeros((output_height, output_width, 3), dtype=np.uint8))
 
-            while capture.isOpened():
-                success, image = capture.read()
-                if not success:
-                    break
+            readers = Value('i', 0)
 
-                image.flags.writeable = False
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            inotify_process = Process(target=watch_file, args=(args.output, readers))
+            inotify_process.start()
 
-                detection_results = face_detection.process(image)
-                image.flags.writeable = True
+            while True:
+                while readers.value == 0:
+                    time.sleep(1)
 
-                if detection_results.detections:
+                capture = cv2.VideoCapture(args.input)
+                capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
+                capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
+                capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+                capture.set(cv2.CAP_PROP_FPS, args.fps)
+
+                center_x, center_y = input_width / 2, input_height / 2
+
+                while capture.isOpened() and readers.value > 0:
+                    success, image = capture.read()
+                    if not success:
+                        break
+
                     image.flags.writeable = False
-                    result = detection_results.detections[0]
+                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+                    detection_results = face_detection.process(image)
                     image.flags.writeable = True
-                    result = detection_results.detections[0]
-                    nose_tip = mp_face_detection.get_key_point(result, mp_face_detection.FaceKeyPoint.NOSE_TIP)
-                    new_center_x = nose_tip.x * input_width
-                    new_center_y = nose_tip.y * input_height
-                    center_x = args.smoothness * center_x + (1 - args.smoothness) * new_center_x
-                    center_y = args.smoothness * center_y + (1 - args.smoothness) * new_center_y
 
-                if args.pose:
-                    image.flags.writeable = False
-                    pose_results = pose.process(image)
-                    image.flags.writeable = True
-                    mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=mediapipe.solutions.drawing_styles.get_default_pose_landmarks_style())
+                    if detection_results.detections:
+                        image.flags.writeable = False
+                        result = detection_results.detections[0]
+                        image.flags.writeable = True
+                        result = detection_results.detections[0]
+                        nose_tip = mp_face_detection.get_key_point(result, mp_face_detection.FaceKeyPoint.NOSE_TIP)
+                        new_center_x = nose_tip.x * input_width
+                        new_center_y = nose_tip.y * input_height
+                        center_x = args.smoothness * center_x + (1 - args.smoothness) * new_center_x
+                        center_y = args.smoothness * center_y + (1 - args.smoothness) * new_center_y
 
-                if args.blur_background:
-                    image.flags.writeable = False
-                    result = change_bg.process(image)
-                    image.flags.writeable = True
-                    fgmask = np.expand_dims(result.segmentation_mask, 2)
-                    inverted = cv2.GaussianBlur(image, (25, 25), 0)
-                    image = (image * fgmask + inverted * (1 - fgmask)).astype(np.uint8)
+                    if args.pose:
+                        image.flags.writeable = False
+                        pose_results = pose.process(image)
+                        image.flags.writeable = True
+                        mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS, landmark_drawing_spec=mediapipe.solutions.drawing_styles.get_default_pose_landmarks_style())
+
+                    if args.blur_background:
+                        image.flags.writeable = False
+                        result = change_bg.process(image)
+                        image.flags.writeable = True
+                        fgmask = np.expand_dims(result.segmentation_mask, 2)
+                        inverted = cv2.GaussianBlur(image, (25, 25), 0)
+                        image = (image * fgmask + inverted * (1 - fgmask)).astype(np.uint8)
 
 
-                offset_x = min(max(int(center_x - output_width / 2), 0), input_width - output_width)
-                offset_y = min(max(int(center_y - output_height / 2), 0), input_height - output_height)
-                image = image[offset_y:offset_y + output_height, offset_x:offset_x + output_width]
+                    offset_x = min(max(int(center_x - output_width / 2), 0), input_width - output_width)
+                    offset_y = min(max(int(center_y - output_height / 2), 0), input_height - output_height)
+                    image = image[offset_y:offset_y + output_height, offset_x:offset_x + output_width]
 
-                camera.send(image)
-                camera.sleep_until_next_frame()
+                    camera.send(image)
+                    camera.sleep_until_next_frame()
+
+                capture.release()
+                camera.send(np.zeros((output_height, output_width, 3), dtype=np.uint8))
+
+            mp_value.value = -100
+            inotify_process.join()
 
 if __name__ == "__main__":
     main()

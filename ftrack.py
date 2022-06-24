28ec1318ada049780a6b66408b05828391c07d9f
Robin Sonnabend <robin@fsmpi.rwth-aachen.de>
2022-06-24 18:15:54 +0200

    Add support for multiple input cameras

--- ftrack.py
+++ ftrack.py
@@ -40,17 +40,30 @@ def draw_point(x, y, image, color, size):
     image[min_y:max_y,min_x:max_x] = color
 
 
+def capture_image(capture, face_detection):
+    success, image = capture.read()
+    if not success:
+        raise ValueError(f"Cannot read image from device {capture}")
+
+    image.flags.writeable = False
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    detection_results = face_detection.process(image)
+    image.flags.writeable = True
+    return image, detection_results
+
+
 def main():
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", "-i", default="/dev/video0", help="Input device path")
+    parser.add_argument("--inputs", "-i", nargs="+", default=["/dev/video0"], help="Input device paths")
     parser.add_argument("--output", "-o", default="/dev/video2", help="Output device path")
     parser.add_argument("--input-size", type=int, nargs=2, default=(1920, 1080), help="Resolution of input video")
     parser.add_argument("--output-size", type=int, nargs=2, default=(1280, 720), help="Resolution of output video")
     parser.add_argument("--fps", type=int, default=30, help="Frames per second for input and output.")
     parser.add_argument("--smoothness", type=float, default=0.95, help="Tracking smoothing factor.")
-    parser.add_argument("--zoom-smoothness", type=float, default=0.95, help="Tracking smoothing factor.")
-    parser.add_argument("--zoom-target", type=float, default=1.0, help="Target value for how far to zoom in.")
+    parser.add_argument("--zoom-smoothness", type=float, default=0.975, help="Tracking smoothing factor.")
+    parser.add_argument("--zoom-target", type=float, default=2.0, help="Target value for how far to zoom in.")
     parser.add_argument("--blur-background", action="store_true", help="Blur background")
     parser.add_argument("--pose", action="store_true", help="Pose detection")
 
@@ -63,17 +76,19 @@ def main():
     min_zoom_factor = 1
     max_zoom_factor = min(input_width / output_width, input_height / output_height)
 
-    capture = cv2.VideoCapture(args.input)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
-    capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-    capture.set(cv2.CAP_PROP_FPS, args.fps)
+    for input_device in args.inputs:
+        print(f"Testing input device {input_device}")
+        capture = cv2.VideoCapture(input_device)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
+        capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+        capture.set(cv2.CAP_PROP_FPS, args.fps)
 
-    success, frame1 = capture.read()
-    if not success:
-        print(f"Failed to read frame from source: {args.input}")
-        return
-    capture.release()
+        success, frame1 = capture.read()
+        if not success:
+            print(f"Failed to read frame from source: {input_input}")
+            return
+        capture.release()
 
 
     with mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.5) as face_detection, mp_background.SelfieSegmentation() as change_bg, mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
@@ -91,27 +106,46 @@ def main():
                 while readers.value == 0:
                     time.sleep(1)
 
-                capture = cv2.VideoCapture(args.input)
-                capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
-                capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
-                capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-                capture.set(cv2.CAP_PROP_FPS, args.fps)
+                captures = []
+                for input_device in args.inputs:
+                    capture = cv2.VideoCapture(input_device)
+                    capture.set(cv2.CAP_PROP_FRAME_WIDTH, input_width)
+                    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, input_height)
+                    capture.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+                    capture.set(cv2.CAP_PROP_FPS, args.fps)
+                    captures.append(capture)
 
                 center_x, center_y = input_width / 2, input_height / 2
                 zoom_factor = (min_zoom_factor + max_zoom_factor) / 2
 
-                while capture.isOpened() and readers.value > 0:
-                    success, image = capture.read()
-                    if not success:
-                        break
+                current_capture = None
+                frames_on_current = 0
 
-                    image.flags.writeable = False
-                    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-                    detection_results = face_detection.process(image)
-                    image.flags.writeable = True
-
-                    if detection_results.detections:
+                while all(capture.isOpened() for capture in captures) and readers.value > 0:
+                    found = False
+                    old_capture = current_capture
+                    if current_capture is not None:
+                        image, detection_results = capture_image(current_capture, face_detection)
+                        old_image = image
+                        frames_on_current += 1
+                        if detection_results.detections:
+                            found = True
+                    if (not found and frames_on_current >= args.fps) or current_capture is None:
+                        for capture in captures:
+                            if capture != current_capture:
+                                image, detection_results = capture_image(capture, face_detection)
+                                if detection_results.detections and any(d.score[0] > 0.9 for d in detection_results.detections):
+                                    print(f"Switching to {capture}")
+                                    found = True
+                                    current_capture = capture
+                                    capture.set(cv2.CAP_PROP_AUTOFOCUS, 0)
+                                    capture.set(cv2.CAP_PROP_AUTOFOCUS, 1)
+                                    center_x = None
+                                    frames_on_current = 0
+                                    break
+                    if not found:
+                        image = old_image
+                    if found:
                         image.flags.writeable = False
                         result = detection_results.detections[0]
                         image.flags.writeable = True
@@ -133,9 +167,14 @@ def main():
                         bbox_height = bbox.height
                         new_zoom_factor = args.zoom_target * max(bbox_width * input_width, bbox_height * input_height) / min(output_width, output_height)
 
-                        center_x = args.smoothness * center_x + (1 - args.smoothness) * new_center_x
-                        center_y = args.smoothness * center_y + (1 - args.smoothness) * new_center_y
-                        zoom_factor = max(min(args.zoom_smoothness * zoom_factor + (1 - args.zoom_smoothness) * new_zoom_factor, max_zoom_factor), min_zoom_factor)
+                        if center_x is not None:
+                            center_x = args.smoothness * center_x + (1 - args.smoothness) * new_center_x
+                            center_y = args.smoothness * center_y + (1 - args.smoothness) * new_center_y
+                            zoom_factor = max(min(args.zoom_smoothness * zoom_factor + (1 - args.zoom_smoothness) * new_zoom_factor, max_zoom_factor), min_zoom_factor)
+                        else:
+                            center_x = new_center_x
+                            center_y = new_center_y
+                            zoom_factor = new_zoom_factor
 
                     if args.pose:
                         image.flags.writeable = False
@@ -161,7 +200,8 @@ def main():
                     camera.send(image)
                     camera.sleep_until_next_frame()
 
-                capture.release()
+                for capture in captures:
+                    capture.release()
                 camera.send(np.zeros((output_height, output_width, 3), dtype=np.uint8))
 
             mp_value.value = -100
